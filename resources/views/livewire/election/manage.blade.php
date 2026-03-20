8e821705799bcf57027f9b3af49ce7ffc5587980
Mike-Berg Sitsofe Afu <mikebergafu@gmail.com>
2026-03-20 06:31:57 +0000

    Change clear device icon to trash icon

--- resources/views/livewire/election/manage.blade.php
+++ resources/views/livewire/election/manage.blade.php
@@ -330,7 +330,7 @@
                                         @if($voter->device_registered)
                                             <button wire:click="clearDeviceFingerprint({{ $voter->id }})" wire:confirm="Clear device registration for {{ $voter->name }}?" class="text-orange-500 hover:text-orange-700 transition-colors" title="Clear device">
                                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                                                 </svg>
                                             </button>
                                         @endif
